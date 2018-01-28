e876fb5e55fa2ebf00e24897426dd646cf2440cc
Trac Nguyen <nguyendtrac@gmail.com>
2018-01-28 20:48:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,12 @@ def take_a_number(queue, name)
     puts "Welcome, #{queue[-1]}. You are number #{queue.length} in line."
   end
 end
+
+def now_serving(queue)
+  if queue.length == 0
+    puts "There is nobody waiting to be served!"
+  elsif queue.length != 0
+    puts "Currently serving #{queue[0]}"
+    queue.shift
+  end
+end
